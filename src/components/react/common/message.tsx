ae02ba3ff77b0542d52f5d32fd835a93159804bc
aratius <arata1129matsu@icloud.com>
2021-06-13 16:31:05 +0900

    テキストを並べる

--- src/components/react/common/message.tsx
+++ src/components/react/common/message.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { getTime } from '../../utils/timeFormatter';
 
 interface Props {
-  messageData: JSON
+  messageData: {
+    text: string,
+    ts: string,
+    user_profile: {
+      image_72: string
+    },
+    blocks: Array<{
+      elements: Array<{
+        elements: Array<{
+          text: string,
+          url: string
+        }>
+      }>
+    }>
+  }
 }
 
 /**
@@ -10,11 +25,61 @@ interface Props {
 export default class Message extends React.Component<Props> {
 
   render() {
-    console.log(this.props.messageData);
+    // メッセージ一つに詰まってる情報すべて
+    const data = this.props.messageData
+    console.log(data);
+
+    // フォーマットした時間
+    const time = getTime(data.ts)
+
+    // iconのURL ない場合はダミーをはめる
+    const icon_url = data.user_profile ? data.user_profile.image_72 : "https://placehold.jp/100x100.png"
 
     return (
-      <>
-      </>
+      <section>
+        {/* アイコン */}
+        <h2>
+          <img src={icon_url} alt="" />
+        </h2>
+
+        <div>
+          {/* 名前と時間のhead */}
+          <div>
+            <p></p>
+            <p>{time}</p>
+          </div>
+
+          {/* メッセージボディ */}
+          <p>
+            {data.blocks.map((block, key1) => {
+              return (
+                <React.Fragment key={key1}>
+                  {block.elements.map((element, key2)=> {
+                    return (
+                      <React.Fragment key={key2}>
+                        {element.elements.map((el, key3) => {
+                          return (
+                            <React.Fragment key={key3}>
+                              {el.text &&
+                                <span>{el.text}</span>
+                              }
+                              {el.url &&
+                                <a href={el.url}>{el.url}</a>
+                              }
+                              <br/>
+                            </React.Fragment>
+                          )
+                        })}
+                      </React.Fragment>
+                    )
+                  })}
+                </React.Fragment>
+              )
+            })}
+          </p>
+        </div>
+
+      </section>
     )
   }
 
---
+++ src/components/utils/timeFormatter.js
@@ -0,0 +1,13 @@
+import moment from 'moment'
+
+moment.lang('ja', {
+  weekdays: ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
+  weekdaysShort: ["日","月","火","水","木","金","土"],
+});
+
+export const getTime = (ts)=> {
+  // const unix = ts.
+  const m = moment.unix(Math.round(Number(ts)))
+
+  return m.format("HH:mm (ddd)")
+}
