6d71b4d720bea0a4df626c5f314e7d471d44fbc1
arata <aualrxse@icloud.com>
2021-08-01 20:28:00 +0900

    ファイルのディレクトリ選択

--- src/components/react/slackView.tsx
+++ src/components/react/slackView.tsx
@@ -16,41 +16,78 @@ export default class SlackView extends React.Component{
     }
   }
 
-  handleReadFile = (e: any) => {
+  handleReadFile = async (e: any) => {
+    const messageTasks: any = []
+    console.log(e.target.files.length);
+
     for(let i = 0; i < e.target.files.length; i++) {
-      const reader = new FileReader()
-      reader.onload = (event: any) => {
-        const messages = JSON.parse(event.target.result);
-        const data = this.state.messageData.length > 0 ? messages.concat(this.state.messageData) : messages
 
-        this.setState({
-          ...this.state,
-          messageData: data
-        })
-      }
+      messageTasks.push(new Promise<any>((res, rej) => {
 
-      reader.readAsText(e.target.files[i])
+        const reader = new FileReader()
+        reader.onload = (event: any) => {
+          const msgs = JSON.parse(event.target.result);
+
+          res(msgs)
+        }
+
+        reader.readAsText(e.target.files[i])
+
+      }))
     }
 
-    console.log(this.state.messageData);
+    const files = await Promise.all(messageTasks)
+    const messages: any = []
+    for(const i in files) {
+      const file: any = files[i]
+      for(const j in file) {
+        messages.push(file[j])
+      }
+    }
+
+    if(this.state.messageData.length > 0) messages.concat(this.state.messageData)
+
+    this.setState({
+      ...this.state,
+      messageData: messages
+    })
 
   }
 
   render() {
 
+    // TODO: チャンネルごとに分けて、日付順でソート
+    console.log(this.state.messageData);
+
     return (
       <>
         {/* slackぽい カードの外枠 */}
         <div>
-          <input type="file" multiple onChange={this.handleReadFile} />
+          {/* ts的にエラーが出る */}
+          {/* ディレクトリ */}
+          <input
+            type="file"
+            name="hello"
+            /* @ts-expect-error */  // エラーを強制的に黙らせる https://qiita.com/minorin22/items/0a8354a81039ea3700a2
+            webkitdirectory=""
+            directory=""
+            multiple
+            onChange={this.handleReadFile}
+          />
+          {/* ファイル選択 */}
+          <input
+            type="file"
+            name="hello"
+            multiple
+            onChange={this.handleReadFile}
+          />
 
           {/* メッセージあるだけループ回す */}
           {/* TODO: 日付順に並び替え */}
-          {/* 授業別ならタブ分ける
-           */}
+          {/* 授業別ならタブ分ける */}
           <ul>
             {this.state.messageData &&
-            this.state.messageData.length &&
+            this.state.messageData.length > 0 &&
             this.state.messageData.map((data: any, key: any) => {
               return(
                 <React.Fragment key={key}>
