e6e7c2baacecc50a0ca3e950597e3f7f6cbf71a2
arata <aualrxse@icloud.com>
2021-08-01 18:45:25 +0900

    moment tsエラー解決

--- src/components/utils/timeFormatter.ts
+++ src/components/utils/timeFormatter.ts
@@ -1,12 +1,12 @@
 import moment from 'moment'
 
 // tsでややこしいからjsにする
-moment.lang('ja', {
+moment.updateLocale('ja', {
   weekdays: ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
   weekdaysShort: ["日","月","火","水","木","金","土"],
 });
 
-export const getTime = (ts)=> {
+export const getTime = (ts: string): string => {
   // const unix = ts.
   const m = moment.unix(Math.round(Number(ts)))
 
