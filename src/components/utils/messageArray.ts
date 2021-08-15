58225e4d82f76a437922b5c2645fcbcad9832600
arata <aualrxse@icloud.com>
2021-08-15 22:16:38 +0900

    ファイル構成整理

---
+++ src/components/utils/messageArray.ts
@@ -0,0 +1,25 @@
+import { Blocks, MessageElement } from "../config/interfaces"
+
+export const getMessageArray = (blocks: Blocks[]): MessageElement[] => {
+  // 返す値
+  const messageArray: MessageElement[] = []
+  for(const i in blocks){
+    const block = blocks[i]
+    if(!("elements" in block)) return null
+    for(const j in block.elements) {
+      const el1 = block.elements[j]
+      if(!("elements" in el1)) return null
+      for(const k in el1.elements) {
+        const el = el1.elements[k]
+        messageArray.push({
+          type: el.type,
+          text: el.text,
+          url: el.url,
+          channel_id: el.channel_id
+        })
+      }
+    }
+  }
+
+  return messageArray
+}
