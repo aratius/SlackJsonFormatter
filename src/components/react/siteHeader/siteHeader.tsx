3e45ab8d8ebc6536250acb001dcf0fac38f6c8a2
arata <aualrxse@icloud.com>
2021-08-16 00:03:10 +0900

    サイトヘッダー

--- src/components/react/siteHeader/siteHeader.tsx
+++ src/components/react/siteHeader/siteHeader.tsx
@@ -6,7 +6,13 @@ export default class SiteHeader extends Component {
     render() {
         return (
             <div className={styles.container}>
-                <h1>Slack Formatter</h1>
+                <h1>
+                    <span className={styles.logo}></span>
+                    <span className={styles.title}>Slack Formatter</span>
+                </h1>
+                <p className={styles.help}>
+
+                </p>
             </div>
         )
     }
