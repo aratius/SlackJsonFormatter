1712943fea477d080ccee537669e21b8e22d5332
arata <aualrxse@icloud.com>
2021-08-15 22:07:30 +0900

    必要そうなコンポーネント準備

---
+++ src/react/components/channelList/channelCard.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class ChannelCard extends Component {
+
+    render() {
+        return (
+            <></>
+        )
+    }
+
+}
---
+++ src/react/components/channelList/channelList.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+/**
+ * チャンネル一覧
+ */
+export default class ChannelList extends Component {
+
+    render() {
+        return (
+            <></>
+        )
+    }
+
+}
