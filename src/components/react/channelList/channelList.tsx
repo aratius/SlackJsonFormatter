434852da52c9b026c14ff4b908dc45e8013b1f6a
arata <aualrxse@icloud.com>
2021-08-16 01:30:15 +0900

    バツボタン

--- src/components/react/channelList/channelList.tsx
+++ src/components/react/channelList/channelList.tsx
@@ -16,25 +16,82 @@ export default class ChannelList extends Component<Props> {
             <div className={styles.container}>
                 <h3 className={styles.title}>Channels</h3>
                 <ul className={styles.channelList}>
-                    <li className={styles.channelList__item}><span>#test_channeltest_channeltest_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
-                    <li className={styles.channelList__item}><span>#test_channel</span></li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channeltest_channeltest_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
+                    <li className={styles.channelList__item}>
+                        <span>#test_channel</span>
+                        <a href="#" className={styles.channelList__item__delete}></a>
+                    </li>
                 </ul>
             </div>
         )
