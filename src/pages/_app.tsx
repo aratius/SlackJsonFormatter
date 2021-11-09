787dde0f767680d36e423c936e8fe1a24e7fcc00
aratius <arata1129matsu@icloud.com>
2021-11-10 01:27:10 +0900

    SP非対応文言

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/global.scss'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from  '../components/react/layout'
-import { Component } from 'react'
+import { useEffect } from 'react';
+import router from "next/router"
 
 export default function App({ Component, pageProps }: any) {
+
+  useEffect((): void => {
+    if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+      alert("SP非対応です。PCでご覧ください。")
+    }
+  })
+
   return(
     <Layout>
       <Component {...pageProps} />
